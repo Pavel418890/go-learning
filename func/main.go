7705301fc8e726e7ca33b6e7743833d78633c309
Pavel Lots <pavel418890@gmail.com>
2022-04-25 09:59:08 +0000

    finish func section

--- func/main.go
+++ func/main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"math"
+	"os"
+	"strconv"
+	"strings"
 )
 
 func cube(a float64) float64 {
@@ -23,7 +26,50 @@ func f1(n uint) (int, int) {
 	return fact, sum
 }
 
+func myFunc(n string) int {
+	total := 0
+	for i := 1; i < 4; i++ {
+		nn, err := strconv.Atoi(strings.Repeat(n, i))
+		if err != nil {
+			fmt.Printf("Cannot convert %q to a string\n", n)
+			os.Exit(1)
+		} else {
+			total += nn
+		}
+
+	}
+	return total
+}
+func sum(n ...int) (s int) {
+	for _, v := range n {
+		s += v
+	}
+	return s
+}
+
+func searchItem(s []string, b string) bool {
+	for i := 0; i < len(s); i++ {
+		if s[i] == b {
+			return true
+		}
+	}
+	return false
+}
+
+func f2(n int) {
+	fmt.Println(n)
+}
 func main() {
 	fmt.Println(cube(5))
 	fmt.Println(f1(23))
+	fmt.Println(myFunc("5"))
+	fmt.Println(sum(1, 2, 3, 4, 5))
+	animals := []string{"lion", "tiger", "bear"}
+	result := searchItem(animals, "bear")
+	fmt.Println(result) // => true
+	result = searchItem(animals, "pig")
+	fmt.Println(result) // => false
+	x := f2
+	fmt.Printf("%T\n", x)
+	x(8)
 }
